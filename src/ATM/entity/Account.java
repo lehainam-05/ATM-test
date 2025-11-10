652696e00044a5353e20d71bf3d5ddc07d71dbe6
HN <lehainam124@gmail.com>
2025-11-10 22:57:42 +0700

    third commit

--- src/ATM/entity/Account.java
+++ src/ATM/entity/Account.java
@@ -143,15 +143,15 @@ public class Account implements Serializable {
         this.transactions = transactions;
     }
 
-//    @Override
-//    public String toString() {
-//        return "Account{" +
-//                "accountNumber='" + accountNumber + '\'' +
-//                ", accountHolderName='" + accountHolderName + '\'' +
-//                ", balance=" + balance +
-//                ", totalTransactions=" + transactions.size() +
-//                '}';
-//    }
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", accountHolderName='" + accountHolderName + '\'' +
+                ", balance=" + balance +
+                ", totalTransactions=" + transactions.size() +
+                '}';
+    }
 
     // @Override
     // /**
